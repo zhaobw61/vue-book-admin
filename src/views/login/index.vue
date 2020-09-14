f39e00370c1b16767e9bb5b3e447360fe3cdda13
zhaobowen <bowen.zhao@chiefclouds.com>
2020-09-14 19:37:14 +0800

    去掉密码长度限制

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -87,11 +87,7 @@ export default {
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('密码不能少于6位'))
-      } else {
-        callback()
-      }
+      callback()
     }
     return {
       loginForm: {
