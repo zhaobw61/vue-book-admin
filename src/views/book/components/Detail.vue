8ae597304fd059a5c9cb50776fd98d1d67119721
zhaobowen <git>
2020-09-16 15:46:10 +0800

    编辑详情页面

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>isEdit: {{isEdit}}</div>
+    <el-fom ref="postForm" :model="postForm">
+        <sticky :class-name="'sub-navbar' + postForm.status">
+            <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
+            <el-button
+            v-loading="loading"
+            @click="submitForm"
+            type="success"
+            style="marign-left: 10px"
+            >{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
+        </sticky>
+    </el-fom>
 </template>
 
 <script>
+import Sticky from '../../../components/Sticky/index'
+import { constants } from 'zlib';
 export default {
+    components: { Sticky },
     props: {
         isEdit: Boolean
+    },
+    data() {
+        return {
+            loading: false,
+            postForm: {
+                status: 'draft'
+            }
+        }
+    },
+    methods: {
+        submitForm() {
+            this.loading = true;
+        },
+        showGuide() {
+            console.log('show asdasd');
+        }
     }
 }
 </script>
