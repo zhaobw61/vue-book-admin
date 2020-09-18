d70fb84609b9d6b7464c7cd6b455410d405816fa
zhaobowen <bowen.zhao@chiefclouds.com>
2020-09-18 14:17:51 +0800

    添加警告组件

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,6 +1,6 @@
 <template>
     <el-fom ref="postForm" :model="postForm">
-        <sticky :class-name="'sub-navbar' + postForm.status">
+        <sticky :class-name="'sub-navbar'">
             <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
             <el-button
             v-loading="loading"
@@ -9,14 +9,26 @@
             style="marign-left: 10px"
             >{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
         </sticky>
+        <div class="detail-container">
+            <el-row>
+                <Warning />
+                <el-col :span="24">
+
+                </el-col>
+                <el-col :span="24">
+
+                </el-col>
+            </el-row>
+        </div>
     </el-fom>
 </template>
 
 <script>
 import Sticky from '../../../components/Sticky/index'
+import Warning from './Warning.vue'
 import { constants } from 'zlib';
 export default {
-    components: { Sticky },
+    components: { Sticky, Warning },
     props: {
         isEdit: Boolean
     },
@@ -40,5 +52,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .detail-container {
+        padding: 40px 50px 20px;
+    }
 </style>
---
+++ src/views/book/components/Warning.vue
@@ -0,0 +1,6 @@
+<template>
+    <aside>
+        上传电子书分为两步
+        获取更多开发指引
+    </aside>
+</template>
