a59522be6b74b8f453bf795c32c91ceb77e08e31
zhaobowen <bowen.zhao@chiefclouds.com>
2020-10-19 11:50:41 +0800

    修复bug

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,60 +1,92 @@
 <template>
-    <el-fom ref="postForm" :model="postForm">
-        <sticky :class-name="'sub-navbar'">
-            <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
-            <el-button
-            v-loading="loading"
-            type="success"
-            style="marign-left: 10px"
-            @click="submitForm"
-            >{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
-        </sticky>
-        <div class="detail-container">
-            <el-row>
-                <Warning />
-                <el-col :span="24">
-                    <ebook-upload />
-                </el-col>
-                <el-col :span="24">
-
-                </el-col>
-            </el-row>
-        </div>
-    </el-fom>
+  <el-form ref="postForm" :model="postForm">
+    <sticky :class-name="'sub-navbar'">
+      <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
+      <el-button v-loading="loading" type="success" style="marign-left: 10px" @click="submitForm">{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
+    </sticky>
+    <div class="detail-container">
+      <el-row>
+        <warning />
+        <el-col :span="24">
+          <ebook-upload :file-list="fileList" :disabled="isEdit" @onSuccess="onUploadSuccess" @onRemove="onUploadRemove" />
+        </el-col>
+        <el-col :span="24">
+          <el-form-item prop="title">
+            <MdInput v-model="postForm.title" :maxlength="100" name="name" required>
+              书名
+            </MdInput>
+          </el-form-item>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="作者：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.author"
+                  placeholder="作者"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="出版社：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.publisher"
+                  placeholder="出版社"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </el-form>
 </template>
 
 <script>
-import Sticky from '../../../components/Sticky/index'
-import Warning from './Warning.vue'
-import EbookUpload from '../../../components/EbookUpload/index'
-import { constants } from 'zlib';
+import Sticky from '../../../components/Sticky'
+import Warning from './Warning'
+import EbookUpload from '../../../components/EbookUpload'
+import MdInput from '../../../components/MDinput'
 
 export default {
-    components: { Sticky, Warning, EbookUpload },
-    props: {
-        isEdit: Boolean
-    },
-    data() {
-        return {
-            loading: false,
-            postForm: {
-                status: 'draft'
-            }
-        }
-    },
-    methods: {
-        submitForm() {
-            this.loading = true;
-        },
-        showGuide() {
-            console.log('show asdasd');
-        }
+  components: { MdInput, Sticky, Warning, EbookUpload },
+  props: {
+    isEdit: Boolean
+  },
+  data() {
+    return {
+      loading: false,
+      postForm: {
+        ebook_uri: ''
+      },
+      fileList: [],
+      labelWidth: '120px'
     }
+  },
+  methods: {
+    onUploadSuccess() {
+      console.log('onUploadSuccess')
+    },
+    onUploadRemove() {
+      console.log('onUploadRemove')
+    },
+    submitForm() {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
+    },
+    showGuide() {
+      console.log('show guide...')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-    .detail-container {
-        padding: 40px 50px 20px;
+  .detail-container {
+    padding: 40px 50px 20px;
+    .preview-img {
+      width: 200px;
+      height: 270px;
     }
+  }
 </style>
